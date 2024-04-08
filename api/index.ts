9a9b590dd9f77f52c92d58a18cc06c1639c7965c
momanyisamuel <momanyisamuel48@gmail.com>
2024-04-08 18:22:09 -0400

    test deploy

---
+++ api/index.ts
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
